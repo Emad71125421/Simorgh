2a5311d59fed45f2c71656ca3f27a5ee80041e1b
Emad71125421 <emadkarami27@gmail.com>
2026-09-18 22:54:19 +0330

    Check and create data directory for database
    Ensure the data directory exists before initializing the database.

--- src/db.js
+++ src/db.js
@@ -1,7 +1,11 @@
 const path = require("path");
+const fs = require("fs");
 const Database = require("better-sqlite3");
 
-const db = new Database(path.join(__dirname, "..", "data", "simorgh.db"));
+const dataDir = path.join(__dirname, "..", "data");
+if (!fs.existsSync(dataDir)) fs.mkdirSync(dataDir, { recursive: true });
+
+const db = new Database(path.join(dataDir, "simorgh.db"));
 db.pragma("journal_mode = WAL");
 db.pragma("foreign_keys = ON");
 
